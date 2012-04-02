b740d1cd27bfda332d574c1870f7970d090baf46
Astrid Smith <Astrid@xrtc.net>
2012-04-01 21:20:48 -0700

    Whitespace, reformatting

--- m68k-stub.c
+++ m68k-stub.c
@@ -1,9 +1,9 @@
 /****************************************************************************
 
-		THIS SOFTWARE IS NOT COPYRIGHTED  
-   
+		THIS SOFTWARE IS NOT COPYRIGHTED
+
    HP offers the following for use in the public domain.  HP makes no
-   warranty with regard to the software or it's performance and the 
+   warranty with regard to the software or it's performance and the
    user accepts the software "AS IS" with all faults.
 
    HP DISCLAIMS ANY WARRANTIES, EXPRESS OR IMPLIED, WITH REGARD
@@ -13,22 +13,22 @@
 ****************************************************************************/
 
 /****************************************************************************
- *  Header: remcom.c,v 1.34 91/03/09 12:29:49 glenne Exp $                   
+ *  Header: remcom.c,v 1.34 91/03/09 12:29:49 glenne Exp $
  *
- *  Module name: remcom.c $  
+ *  Module name: remcom.c $
  *  Revision: 1.34 $
  *  Date: 91/03/09 12:29:49 $
  *  Contributor:     Lake Stevens Instrument Division$
- *  
+ *
  *  Description:     low level support for gdb debugger. $
  *
  *  Considerations:  only works on target hardware $
  *
  *  Written by:      Glenn Engel $
- *  ModuleState:     Experimental $ 
+ *  ModuleState:     Experimental $
  *
  *  NOTES:           See Below $
- * 
+ *
  *  To enable debugger support, two things need to happen.  One, a
  *  call to set_debug_traps() is necessary in order to allow any breakpoints
  *  or error conditions to be properly intercepted and reported to gdb.
@@ -39,15 +39,15 @@
  *  there either should be a standard breakpoint instruction, or the protocol
  *  should be extended to provide some means to communicate which breakpoint
  *  instruction is in use (or have the stub insert the breakpoint).
- *  
+ *
  *  Some explanation is probably necessary to explain how exceptions are
  *  handled.  When an exception is encountered the 68000 pushes the current
  *  program counter and status register onto the supervisor stack and then
  *  transfers execution to a location specified in it's vector table.
  *  The handlers for the exception vectors are hardwired to jmp to an address
- *  given by the relation:  (exception - 256) * 6.  These are decending 
+ *  given by the relation:  (exception - 256) * 6.  These are decending
  *  addresses starting from -6, -12, -18, ...  By allowing 6 bytes for
- *  each entry, a jsr, jmp, bsr, ... can be used to enter the exception 
+ *  each entry, a jsr, jmp, bsr, ... can be used to enter the exception
  *  handler.  Using a jsr to handle an exception has an added benefit of
  *  allowing a single handler to service several exceptions and use the
  *  return address as the key differentiation.  The vector number can be
@@ -56,50 +56,50 @@
  *  exception number and push it on the stack in place of the return address.
  *  The external function exceptionHandler() is
  *  used to attach a specific handler to a specific m68k exception.
- * 
+ *
  *  Because gdb will sometimes write to the stack area to execute function
  *  calls, this program cannot rely on using the supervisor stack so it
- *  uses it's own stack area reserved in the int array remcomStack.  
- * 
+ *  uses it's own stack area reserved in the int array remcomStack.
+ *
  *************
  *
  *    The following gdb commands are supported:
- * 
+ *
  * command          function                               Return value
- * 
+ *
  *    g             return the value of the CPU registers  hex data or ENN
  *    G             set the value of the CPU registers     OK or ENN
- * 
+ *
  *    mAA..AA,LLLL  Read LLLL bytes at address AA..AA      hex data or ENN
  *    MAA..AA,LLLL: Write LLLL bytes at address AA.AA      OK or ENN
- * 
+ *
  *    c             Resume at current address              SNN   ( signal NN)
  *    cAA..AA       Continue at address AA..AA             SNN
- * 
+ *
  *    s             Step one instruction                   SNN
  *    sAA..AA       Step one instruction from AA..AA       SNN
- * 
+ *
  *    k             kill
  *
  *    ?             What was the last sigval ?             SNN   (signal NN)
- * 
- * All commands and responses are sent with a packet which includes a 
- * checksum.  A packet consists of 
- * 
+ *
+ * All commands and responses are sent with a packet which includes a
+ * checksum.  A packet consists of
+ *
  * $<packet info>#<checksum>.
- * 
+ *
  * where
  * <packet info> :: <characters representing the command or response>
  * <checksum>    :: < two hex digits computed as modulo 256 sum of <packetinfo>>
- * 
+ *
  * When a packet is received, it is first acknowledged with either '+' or '-'.
  * '+' indicates a successful transfer.  '-' indicates a failed transfer.
- * 
+ *
  * Example:
- * 
+ *
  * Host:                  Reply:
  * $m0,10#2a               +$00010203040506070809101112131415#42
- * 
+ *
  ****************************************************************************/
 
 #include <intr.h>
@@ -110,7 +110,7 @@
 
 /************************************************************************
  *
- * external low-level support routines 
+ * external low-level support routines
  */
 typedef void (*ExceptionHook)(int);   /* pointer to function with int parm */
 typedef void (*Function)();           /* pointer to a function */
@@ -135,15 +135,15 @@ initializeRemcomErrorFrame ();
 static char initialized;  /* boolean flag. != 0 means we've been initialized */
 
 int     remote_debug;
-/*  debug >  0 prints ill-formed commands in valid packets & checksum errors */ 
+/*  debug >  0 prints ill-formed commands in valid packets & checksum errors */
 
 static const char hexchars[]="0123456789abcdef";
 
 /* there are 180 bytes of registers on a 68020 w/68881      */
 /* many of the fpa registers are 12 byte (96 bit) registers */
 #define NUMREGBYTES 180
-enum regnames {D0,D1,D2,D3,D4,D5,D6,D7, 
-               A0,A1,A2,A3,A4,A5,A6,A7, 
+enum regnames {D0,D1,D2,D3,D4,D5,D6,D7,
+               A0,A1,A2,A3,A4,A5,A6,A7,
                PS,PC,
                FP0,FP1,FP2,FP3,FP4,FP5,FP6,FP7,
                FPCONTROL,FPSTATUS,FPIADDR
@@ -187,7 +187,7 @@ static int* stackPtr = &remcomStack[STACKSIZE/sizeof(int) - 1];
 
 /*
  * In many cases, the system will want to continue exception processing
- * when a continue command is given.  
+ * when a continue command is given.
  * oldExceptionHook is a function to invoke in this case.
  */
 
@@ -199,9 +199,6 @@ jmp_buf remcomEnv;
 /***************************  ASSEMBLY CODE MACROS *************************/
 /* 									   */
 
-#define SAVE_FP_REGS()
-#define RESTORE_FP_REGS()
-
 void return_to_super();
 void return_to_user();
 
@@ -209,33 +206,31 @@ asm("
 .text
 .globl return_to_super
 return_to_super:
-        movel   registers+60,%sp /* get new stack pointer */        
-        movel   lastFrame,%a0   /* get last frame info  */              
+        movel   registers+60,%sp /* get new stack pointer */
+        movel   lastFrame,%a0   /* get last frame info  */
         bra     return_to_any
 
 .globl return_to_user
 return_to_user:
-        movel   registers+60,%a0 /* get usp */                          
-        movel   %a0,%usp           /* set usp */				
-        movel   superStack,%sp  /* get original stack pointer */        
+        movel	registers+60,%a0	/* get usp */
+        movel	%a0,%usp		/* set usp */
+        movel	superStack,%sp		/* get original stack pointer */
 
 return_to_any:
-        movel   lastFrame,%a0   /* get last frame info  */              
-        movel   (%a0)+,lastFrame /* link in previous frame     */        
-        addql   #8,%a0           /* skip over pc, vector#*/              
-        movew   (%a0)+,%d0         /* get # of words in cpu frame */       
-        addw    %d0,%a0           /* point to end of data        */       
-        addw    %d0,%a0           /* point to end of data        */       
-        movel   %a0,%a1                                                   
-#                                                                       
-# copy the stack frame                                                  
-copyUserLoop:                                                               
-        movew   -(%a1),-(%sp)                                               
-        dbf     %d0,copyUserLoop                                             
-");                                                                     
-        RESTORE_FP_REGS()                                              
-   asm("   moveml  registers,%d0-%d7/%a0-%a6");			        
-   asm("   rte");  /* pop and go! */                                    
+	movel	lastFrame,%a0		/* get last frame info */
+	movel	(%a0)+,lastFrame	/* link in previous frame */
+	addql	#8,%a0			/* skip over pc, vector# */
+	movew	(%a0)+,%d0		/* get # of words in cpu frame */
+	addw	%d0,%a0			/* point to end of data */
+	addw	%d0,%a0			/* point to end of data */
+	movel	%a0,%a1
+#
+# copy the stack frame
+copyUserLoop:
+        movew   -(%a1),-(%sp)
+        dbf     %d0,copyUserLoop
+	moveml  registers,%d0-%d7/%a0-%a6
+	rte");  /* pop and go! */
 
 #define DISABLE_INTERRUPTS()   asm("         oriw   #0x0700,%sr");
 #define BREAKPOINT() asm("   trap #1");
@@ -265,14 +260,14 @@ extern void _catchException ();
 /* This function is called when an exception occurs.  It translates the
  * return address found on the stack into an exception vector # which
  * is then handled by either handle_exception or a system handler.
- * _catchException provides a front end for both.  
+ * _catchException provides a front end for both.
  *
  * stack on entry:                       stack on exit:
- *   Program counter MSWord              exception # MSWord 
+ *   Program counter MSWord              exception # MSWord
  *   Program counter LSWord              exception # MSWord
- *   Status Register                     
- *   Return Address  MSWord              
- *   Return Address  LSWord             
+ *   Status Register
+ *   Return Address  MSWord
+ *   Return Address  LSWord
  */
 asm("
 .text
@@ -282,14 +277,11 @@ DISABLE_INTERRUPTS();
 asm("
         moveml %d0-%d7/%a0-%a6,registers  /* save registers               */
         movel   lastFrame,%a0   /* last frame pointer */
-");
-SAVE_FP_REGS();        
-asm("
         lea     registers,%a5   /* get address of registers     */
         movel   (%sp)+,%d2         /* pop return address           */
         addl    #1530,%d2        /* convert return addr to       */
         divs    #6,%d2           /*  exception number            */
-        extl    %d2   
+        extl    %d2
 
         moveql  #3,%d3           /* assume a three word frame     */
 
@@ -298,11 +290,11 @@ asm("
         movel   (%sp)+,-(%a0)       /* copy error info to frame buff*/
         movel   (%sp)+,-(%a0)       /* these are never used         */
         moveql  #7,%d3           /* this is a 7 word frame       */
-     
-normal:   
+
+normal:
         movew   (%sp)+,%d1         /* pop status register          */
         movel   (%sp)+,%a4         /* pop program counter          */
-        movew   %d1,%a5@(66)      /* save sr                      */      
+        movew   %d1,%a5@(66)      /* save sr                      */
         movel   %a4,%a5@(68)      /* save pc in _regisers[]       */
         movel   %a4,%a0@-         /* copy pc to frame buffer      */
         movew   %d1,%a0@-         /* copy sr to frame buffer      */
@@ -310,9 +302,9 @@ normal:
         movel   %sp,superStack  /* save supervisor sp          */
 
         andiw   #0x2000,%d1      /* were we in supervisor mode ? */
-        beq     userMode       
+        beq     userMode
         movel   %a7,%a5@(60)      /* save a7                  */
-        bra     saveDone             
+        bra     saveDone
 userMode:
         movel   %usp,%a1          /* save user stack pointer      */
         movel   %a1,%a5@(60)      /* save user stack pointer      */
@@ -560,7 +552,7 @@ DEFINE_INT_HANDLER(handle_buserror)
   longjmp (remcomEnv, 1);
 }
 
-/* this function takes the 68000 exception number and attempts to 
+/* this function takes the 68000 exception number and attempts to
    translate this number into a unix compatible signal value */
 int
 computeSignal (exceptionVector)
@@ -912,7 +904,7 @@ initializeRemcomErrorFrame (void)
   lastFrame->previous = lastFrame;
 }
 
-/* this function is used to set up exception handlers for tracing and 
+/* this function is used to set up exception handlers for tracing and
    breakpoints */
 void
 set_debug_traps ()
